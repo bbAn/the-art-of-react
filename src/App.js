82c91608050ee7e6a19cbdb1310928e9780d5240
bbAn <damaskrose83@gmail.com>
2022-11-27 01:03:41 +0900

    2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const name = '리액트';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>
   );
 }
 
