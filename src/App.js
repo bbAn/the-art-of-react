54be4c8634afb224e015e52cee976181045dfea7
bbAn <damaskrose83@gmail.com>
2022-11-27 01:06:16 +0900

    2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   const name = '리액트';
 
   return (
-    <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>
+    // <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>
+    // 2.4.4 AND 연산자(&&)를 사용한 조건부 렌더링
+    <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>
   );
 }
 
