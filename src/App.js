9b9a46bb48a113202424bb73b38c9de75b950987
bbAn <damaskrose83@gmail.com>
2022-11-27 01:32:32 +0900

    3.3.1 JSX 내부에서 props 렌더링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  // const name = undefined;
-  // return name || '값이 undefined입니다.';
-  
-  // name 값이 undefined일때 보여 주고 싶은 문구가 있다면
-  const name = undefined;
-  return <div> {name || '보여 주고 싶은 문구내용'}</div>;
+const App = () =>{
+  return <MyComponent name="Namy" />;
 }
 
 export default App;
