e96f415e73ad42f8a0ac4f258352e2f10fbd4fb1
bbAn <damaskrose83@gmail.com>
2022-11-27 01:52:15 +0900

    3.3.6 propTypes를 통한 props 검증

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () =>{
-  return <MyComponent>리액트</MyComponent>;
+  // name 값은 무조건 문자 형태로 전달되어야함 숫자인 3으로 전달할 경우 경고 메시지 출력
+  // return <MyComponent name={3}>리액트</MyComponent>;
+  return <MyComponent name="React">리액트</MyComponent>;
+
 }
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,29 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = props => {
-  const { name, children } = props; //비구조화 할당 문법
+const MyComponent = ({ name, children }) => {
   return (
     <div>
-      My name is {name}. <br /> {/* props.을 제외하고 사용할 수 있음 */}
+      My name is {name}. <br />
       children value is {children}.
     </div>
   )
 };
 
-// 함수의 파라미터 부분에도 사용할 수 있으며
-// 함수의 파라미터가 객체라면 그 값을 바로 비구조화해서 사용할 수 있음
-// const MyComponent = ({ name, children }) => {
-//   return (
-//     <div>
-//       My name is {name}. //props.을 제외하고 사용할 수 있음
-//       children value is {children}.
-//     </div>
-//   )
-// };
-//
-
 MyComponent.defaultProps = {
   name: 'default name'
 };
+
+// props의 타입 지정 
+MyComponent.propTypes = {
+  name: PropTypes.string
+}
 
 export default MyComponent;
