5308e89fe1208f9118478622b05d93694b5c32e3
bbAn <damaskrose83@gmail.com>
2022-11-27 14:59:06 +0900

    4.2.3.1 임의 메서드 기본 방식

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -6,6 +6,27 @@ class EventPractice extends Component {
     message: ""
   }
 
+  constructor(props) {
+    super(props);
+    // this가 컴포넌트 자신으로 제대로 가르키기 위해서 메서드를 this와 바인딩해야함
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  };
+  
+  // 임의 메서드
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,22 +36,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={
-            (e) => {
-              this.setState({
-                message: e.target.value
-              })
-            }
-          }
+          onChange={this.handleChange} // 임의 메서드 등록
         />
-        <button onClick={
-          () => {
-            alert(this.state.message);
-            this.setState({
-              message: ''
-            });
-          }
-        }>확인</button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
