b3ffa95eba7e183d8d47579d9583cc635e05482a
bbAn <damaskrose83@gmail.com>
2022-11-27 15:00:36 +0900

    4.2.3.2 Property Initializer Syntax를 사용한 메서드 작성

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -5,22 +5,17 @@ class EventPractice extends Component {
   state = {
     message: ""
   }
-
-  constructor(props) {
-    super(props);
-    // this가 컴포넌트 자신으로 제대로 가르키기 위해서 메서드를 this와 바인딩해야함
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  };
   
-  // 임의 메서드
-  handleChange(e) {
+  //constructor 삭제
+  
+  
+  handleChange = (e) => { // 화살표 함수 형식으로 간단하게 변경
     this.setState({
       message: e.target.value
     });
   }
 
-  handleClick() {
+  handleClick = () => { // 화살표 함수 형식으로 간단하게 변경
     alert(this.state.message);
     this.setState({
       message: ''
@@ -36,7 +31,7 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={this.handleChange} // 임의 메서드 등록
+          onChange={this.handleChange}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
