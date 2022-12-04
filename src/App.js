d3dd21f9daf83328655b37d40811d49a3d37500d
bbAn <damaskrose83@gmail.com>
2022-12-04 18:05:07 +0900

    Revert "5.1.1 예제 컴포넌트 생성"
    This reverts commit 10d5941eab91bfca1a760e63965390f06782916b.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ValidationSample from './ValidationSample';
+import EventPractice from './EventPractice';
 
 const App = () => {
-  return <ValidationSample />;
+  return <EventPractice />;
 }
 
 export default App;
