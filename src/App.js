7528c48ca358888dfd3add0084015b28d15bccd4
bbAn <damaskrose83@gmail.com>
2022-12-10 14:20:38 +0900

    7.3.1 라이프 사이클 예제 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+
+// 랜덤 색상을 생성
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+
+  state = {
+    color:'#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
   render() {
     return (
-      <IterationSample />
+      <div>
+        {/* 버튼을 클릭할 때 마다 handleClick 호출 */}
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        {/* color값을 props로 설정 */}
+        <LifeCycleSample color={this.state.color} />
+      </div>
     );
   }
 }
---
+++ src/LifeCycleSample.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+class LifeCycleSample extends Component {
+  state = {
+    number: 0,
+    color: null,
+  }
+
+  myRef = null; // ref를 설정할 부분
+
+  constructor(props) {
+    super(props);
+    console.log('constructor')
+  }
+
+  // 컴포넌트가 최초 마운트될 때와 업데이트될 때 redder 호출 전에 호출     
+  // 부모에게서 받은 color 값을 state에 동기화
+  static getDerivedStateFromProps(nextProps, prevState) { 
+    console.log('getDerivedStateFromProps');
+    if(nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  // 컴포넌트를 만들고 첫 렌더링을 다 마친 후 실행 
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  // props나 state를 변경했을 때 리렌더링을 시작할지 여부를 지정   
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate', nextProps, nextState);
+    return nextState.number % 10 !==4; // 숫자의 마지막 자리가 4면 리렌더링하지 않음
+  }
+
+  // 컴포넌트가 DOM에서 제거되기 직전에 실행함
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1
+    });
+  }
+
+  // 컴포넌트의 변화를 DOM에 반영하기 바로 직전에 호출하는 메서드
+  // DOM에 변화가 일어나기 직전의 색상 속성을 snapshot값으로 반환해서 componentDidUpdate 에서 조회할 수 있게 함
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    if(prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  // 컴포넌트의 업데이트 작업이 끝난 후 호출하는 메서드 최초 렌더링에서는 호출되지 않음
+  componentDidUpdate(prevProps, prevState, snapShot) {
+    console.log('componentDidUpdate', prevProps, prevState);
+    if(snapShot) {
+      console.log('업데이트되기 직전 색상: ', snapShot);
+    }
+  }
+
+  // render(): 
+  render() {
+    console.log('render');
+
+    const style = {
+      color: this.props.color
+    };
+
+    return (
+      <div>
+        <h1 style={style} ref={ref => this.myRef=ref}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>
+          더하기
+        </button>
+      </div>
+    )
+  }
+}
+
+export default LifeCycleSample;
