a6d9017948e0993dcb10405981be3adf8d09cdb8
bbAn <damaskrose83@gmail.com>
2022-12-10 13:35:58 +0900

    6.3.1 key 설정

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const IterationSample = () => {
   const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map(name => <li>{name}</li>); // currentValue는 name. 새로 생성된 배열을 nameList에 담는다
-  return <ul>{nameList}</ul>;
+  const namesList = names.map((name, index) => <li key={index}>{name}</li>); //currentValue: name, index: index
+  // index를 key로 사용하면 배열이 변경될 때 효율적으로 렌더링 하지 못하기 때문에 고유한 값이 없을 때만 index 값을 key로 사용한다
+ 
+  return <ul>{namesList}</ul>;
 }
 
 export default IterationSample;
