458abb5e71ee58787d5b84d2e7786b35586bdfa4
bbAn <damaskrose83@gmail.com>
2022-12-10 23:11:59 +0900

    8.2 useEffect

--- src/info.js
+++ src/info.js
@@ -1,8 +1,17 @@
-import React,  { useState } from 'react';
+import React,  { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  // 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname
+    });
+  });
 
   const onChangeName = e => {
     setName(e.target.value);
