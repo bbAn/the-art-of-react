7a43ba8994e48227213f5fd5e9bad03c531743cf
bbAn <damaskrose83@gmail.com>
2022-12-10 23:54:51 +0900

    8.2.3 뒷정리하기

--- src/info.js
+++ src/info.js
@@ -6,8 +6,16 @@ const Info = () => {
 
   // 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정
   useEffect(() => {
+    console.log('effect');
     console.log(name);
-  }, [name]); // 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값 설정
+
+    // 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면   
+    // useEffect에서 뒷정리(cleanup) 함수를 반환해 주어야함 
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  }); // 언마운트 때만 뒷정리 함수를 호출하고 싶다면 두 번째 파라미터에 빈 배열을 넣음
 
   const onChangeName = e => {
     setName(e.target.value);
