f44110d043761d363bcff59f60f4b37e06881ce7
bbAn <damaskrose83@gmail.com>
2022-12-11 14:26:49 +0900

    8.3 useReducer

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  // action.type에 따라 다른 작업 수행 
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      // 아무것도 해당되지 않을 때 기존 상태 변환
+    return state;
+  }
+}
 
 const CounterHook = () => {
-  // useState 함수 파라미터에는 상태의 기본값(0)을 넣어줌
-  // 즉 카운터의 기본값(value)을 0으로 설정하겠다는 의미
-  const [value, setValue] = useState(0);
-  // 배열의 첫 번째는 원소의 상태값, 두 번째 원소는 상태 설정 함수
+  // useReducer의 첫 번쨰 파라미터에는 리듀서 함수, 두 번째 파라미터에는 해당 리듀서의 기본 값을 넣음
+  // 이 훅은 state(현재 가리키고 있는 상태) 값과 dispatch(액션을 발생시키는 함수) 함수를 받아옴
+  const [state, dispatch] = useReducer(reducer, {value: 0});  
 
   return(
     <div>
       <p>
-         현재 카운터 값은 <b>{value}</b>입니다.
+         현재 카운터 값은 <b>{state.value}</b>입니다.
       </p>
-      <button onClick={() => setValue(value + 1)}> +1</button>
-      <button onClick={() => setValue(value - 1)}> -1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}> +1</button> {/* dispatch(action) 형태로 함수 안에 파라미터로 액션 값을 넣어주면 리듀서 함수가 호출됨 */}
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}> -1</button>
     </div>
   );
 };
